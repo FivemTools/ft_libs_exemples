761abd38b6ad56431f923a28a1826ee1ba9308b8
Samuel Ds <samuelds.pro@gmail.com>
2018-06-24 20:27:44 +0200

    - Add new path file

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+--
+-- @Project: FiveM Tools
+-- @Author: Samuelds
+-- @License: GNU General Public License v3.0
+--
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+dependencies {}
+
+client_scripts {
+
+    -- Market exemple
+    "src/market/market.client.lua",
+
+}
+
+server_scripts {}
