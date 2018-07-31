d660eb8decb8b357e34435f814d1898b8dcf7ac8
Samuel Ds <samuelds.pro@gmail.com>
2018-07-31 22:34:51 +0200

    Change Citizen.Wait to 5

--- src/tests/init.client.lua
+++ src/tests/init.client.lua
@@ -60,7 +60,7 @@ AddEventHandler('ft_libs:OnClientReady', function()
                     exports.ft_libs:OpenMenu("ft_libs_exemples_test:HomeMenu")
                 end
             end
-            Citizen.Wait(10)
+            Citizen.Wait(5)
         end
     end)
 
